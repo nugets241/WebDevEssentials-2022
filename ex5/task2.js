64b2a2885c6f04b65aba70b9acc40997e41242e0
Ej Sobrepeña <ej.sobrepena@tuni.fi>
2022-03-06 23:06:42 +0200

    Exercise 5 p1

---
+++ ex5/task2.js
@@ -0,0 +1,17 @@
+var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex,colorCode,color) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
+    tabPanels[panelIndex].style.color=color;
+}
+showPanel(0,'lightseagreen', 'black');
